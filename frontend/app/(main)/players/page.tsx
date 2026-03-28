2b3a1514e6a3ed254d42f2a1ef0f7efbf9312157
James Melanson <jmmelanson04@gmail.com>
2026-03-28 17:41:12 -0400

    adding more forced dynamic rendering to get render to cooperate in draft/page and players/page

--- frontend/app/(main)/players/page.tsx
+++ frontend/app/(main)/players/page.tsx
@@ -2,7 +2,7 @@ import { getPlayers } from "@/_lib/api";
 import { PlayerGetParams, PlayerGetResponse } from "@/_lib/types";
 import PlayerTable from "@/components/players/player_table";
 import Pagination from "@/components/ui/pagination";
-
+export const dynamic="force-dynamic"
 export default async function Player({searchParams} : {searchParams: PlayerGetParams}) {
     // Get search params to pass in
     searchParams = await searchParams;
